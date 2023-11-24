5b1111411ce57eaecb9943f330346a6bfb483b10
Trunpy28 <127753383+Trunpy28@users.noreply.github.com>
2023-11-24 22:25:00 +0700

    Init project

---
+++ src/main/java/com/hust/soict/cnpm/group24/CNPMGroup24.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hust.soict.cnpm.group24;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class CNPMGroup24 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
