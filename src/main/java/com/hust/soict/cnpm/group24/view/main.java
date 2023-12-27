b17d80589cd4337acd329a95e0250ea0dae3a8c8
Tung <gamer1412all@gmail.com>
2023-12-27 21:37:17 +0700

    Init screen

---
+++ src/main/java/com/hust/soict/cnpm/group24/view/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hust.soict.cnpm.group24.view;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MainScreen mainScreen = new MainScreen();
+        mainScreen.ShowMain();
+        //LoginScreen.ShowLogin();
+        //SignupScreen.showSignup();
+    }
+}
